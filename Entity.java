8aad004231427f8b633bf68fea06b9af5b173e17
ericgraham3 <eric.graham@gmail.com>
2023-12-08 16:52:21 -0600

    moved the attack method and armor/attack/damage bonuses (and related methods) out of warrior and skeleton classes, into the main entity class

--- Entity.java
+++ Entity.java
@@ -9,11 +9,11 @@ public class Entity {
     private int hitPoints = 10;
     private int wounds = 0;
     private int initiative = 0;
-
+    private int armorBonus;
+    private int attackBonus;
+    private int damageBonus;
     private ArrayList<Item> inventory = new ArrayList<>();
-
     private String description;
-
     private Weapon equippedWeapon;
 
     // constructor
@@ -23,17 +23,20 @@ public class Entity {
     }
     // functional methods
 
-    public void attack(Entity attacker, Entity target, int damageSize){
-        int attackRoll = Mechanics.rollDice(20, 0);
-        if (attackRoll >= target.armorRating){
-            int damageAmount = Mechanics.rollDice(damageSize, 0);
+    public void attack(Entity target){
+        int attackRoll = Mechanics.rollDice(20, this.attackBonus);
+        System.out.println(this.getName() +"'s attack roll is: " +attackRoll);
+        System.out.println(target.getName() +"s armor rating is " +target.getArmorRating());
+        if (attackRoll >= target.getArmorRating()){
+            int damageAmount = Mechanics.rollDice(this.getEquippedWeapon().getDamageSize(), this.damageBonus);
             target.takeDamage(damageAmount);
-            System.out.println(attacker.getName() +" attacks " +target.getName() +" and deals " +damageAmount +" damage to them.");
+            System.out.println(this.getName() +"'s damage amount is: " +damageAmount);
+            System.out.println(this.getName() +" deals " +damageAmount +" damage to " +target.getName()  +" with their " + this.getEquippedWeapon().getItemName());
         }
         else {
-            System.out.println(attacker.getName() +" attacks " +target.getName() +" but misses.");
-            }
+            System.out.println(this.getName() +" attacks " +target.getName() +" with their " + this.getEquippedWeapon().getItemName() +" but misses.");
         }
+    }
     public void takeDamage(int damageTaken){
         this.wounds += damageTaken;
     }
@@ -98,6 +101,54 @@ public class Entity {
 
     public void setInitiative(int initiative) {
         this.initiative = initiative;
+    }
+
+    public int getArmorBonus() {
+        return armorBonus;
+    }
+
+    public void setArmorBonus(int armorBonus) {
+        this.armorBonus = armorBonus;
+    }
+
+    public void increaseArmorBonus(int armorBonus) {
+        this.armorBonus += armorBonus;
+    }
+
+    public void decreaseArmorBonus(int armorBonus) {
+        this.armorBonus -= armorBonus;
+    }
+
+    public int getAttackBonus() {
+        return attackBonus;
+    }
+
+    public void setAttackBonus(int attackBonus) {
+        this.attackBonus = attackBonus;
+    }
+
+    public void increaseAttackBonus(int attackBonus) {
+        this.attackBonus += attackBonus;
+    }
+
+    public void decreaseAttackBonus(int attackBonus) {
+        this.attackBonus -= attackBonus;
+    }
+
+    public int getDamageBonus() {
+        return damageBonus;
+    }
+
+    public void setDamageBonus(int damageBonus) {
+        this.damageBonus = damageBonus;
+    }
+
+    public void increaseDamageBonus(int damageBonus) {
+        this.damageBonus += damageBonus;
+    }
+
+    public void decreaseDamageBonus(int damageBonus) {
+        this.damageBonus -= damageBonus;
     }
 
     public ArrayList<Item> getInventory() {
--- Skeleton.java
+++ Skeleton.java
@@ -7,17 +7,4 @@ public class Skeleton extends Antagonist{
         this.setEquippedWeapon(startingWeapon);
         startingWeapon.setEquipped(true);
     }
-    public void attack(Entity target) {
-        int attackRoll = Mechanics.rollDice(20, 0);
-        System.out.println(this.getName() +"'s attack roll is: " +attackRoll);
-        System.out.println(target.getName() +"s armor rating is " +target.getArmorRating());
-        if (attackRoll >= target.getArmorRating()) {
-            int damageAmount = Mechanics.rollDice(this.getEquippedWeapon().getDamageSize(), 0);
-            System.out.println(this.getName() +"'s damage amount is: " +damageAmount);
-            target.takeDamage(damageAmount);
-            System.out.println(this.getName() + " deals " + damageAmount + " damage to " + target.getName() + " with their " + this.getEquippedWeapon().getItemName());
-        } else {
-            System.out.println(this.getName() + " attacks " + target.getName() + " with their " + this.getEquippedWeapon().getItemName() + " but misses.");
-        }
-    }
 }
